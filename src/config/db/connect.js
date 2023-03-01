6ae961e5817c6381985dcbdbcff2781c14e788e2
seghodavid <56871763+seghodavid@users.noreply.github.com>
2023-03-01 10:43:13 +0100

    DB config:fix

--- src/config/db/connect.js
+++ src/config/db/connect.js
@@ -3,11 +3,11 @@ const mysql = require("mysql");
 const connectDB = () => {
   return mysql.createConnection(
     {
-      host: "127.0.0.1",
+      host: process.env.DATABASE_HOST,
       port: 3306,
-      user: "root",
-      password: "Television19",
-      database: "restful-api",
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
     },
     console.log("Connected to database...")
   );
